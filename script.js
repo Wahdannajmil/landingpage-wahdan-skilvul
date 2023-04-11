ae15e8afdd24266d057a8ec585c7b75f68321ad1
Wahdan Najmil <najmilwahdan@gmail.com>
2023-04-11 09:33:01 +0700

    add script.js and eidit images

--- script.js
+++ script.js
@@ -13,13 +13,16 @@ function handleGetFormData() {
     status: status
   };
 }
+
 function isNumber(input) {
-  return !isNaN(input);
+  return !isNaN(input); //apakah input bukan angka 
 }
+
 function checkboxIsChecked() {
   const status = document.getElementById('status');
   return status.checked;
 }
+
 function validateFormData(formData) {
   if (!formData) {
     return false;
@@ -28,41 +31,8 @@ function validateFormData(formData) {
   if (!isNumber(formData.zipCode)) {
     return false;
   }
-  
   if (!checkboxIsChecked()) {
     return false;
   }
-  
   return true;
 }
-
-
-function submit(event) {
-  // mencegah refresh pada page
-  event.preventDefault();
-  
-  // mendapatkan data formulir
-  const formData = {
-    name: document.getElementById('name').value,
-    city: document.getElementById('city').value,
-    email: document.getElementById('email').value,
-    zipCode: parseInt(document.getElementById('zip-code').value),
-    status: document.getElementById('status').checked
-  };
-  
-  // melakukan validasi formulir
-  const isValid = validateFormData(formData);
-  
-  // menampilkan pesan kesalahan jika formulir tidak valid
-  const warningDiv = document.getElementById('warning');
-  if (!isValid) {
-    warningDiv.innerText = 'Periksa form anda sekali lagi';
-  } else {
-    warningDiv.innerText = '';
-  }
-}
-
-// menghubungkan function submit dengan form pada file HTML
-const form = document.getElementById('myForm');
-form.addEventListener('submit', submit);
-
