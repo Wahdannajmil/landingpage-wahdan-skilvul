4258a4ed546a56bbedbee07fdaf4dddc0b07be52
Wahdan Najmil <najmilwahdan@gmail.com>
2023-04-12 18:09:29 +0700

    finishing landpage

--- script.js
+++ script.js
@@ -1,38 +1,57 @@
+
+
 function handleGetFormData() {
-  const name = document.getElementById('name').value;
-  const city = document.getElementById('city').value;
-  const email = document.getElementById('email').value;
-  const zipCode = document.getElementById('zip-code').value;
-  const status = document.getElementById('status').checked;
-  
-  return {
-    name: name,
-    city: city,
-    email: email,
-    zipCode: zipCode,
-    status: status
-  };
+  const name = document.getElementById("name").value;
+  const city = document.getElementById("city").value;
+  const email = document.getElementById("email").value;
+  const zipCode = document.getElementById("zip-code").value;
+  const status = document.getElementById("status").checked;
+  return { name, city, email, zipCode, status };
 }
 
-function isNumber(input) {
-  return !isNaN(input); //apakah input bukan angka 
+function isNumber(str) {
+  return !isNaN(str) && !isNaN(parseFloat(str));
 }
 
 function checkboxIsChecked() {
-  const status = document.getElementById('status');
-  return status.checked;
+  const status = document.getElementById("status");
+  if (status.checked) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 function validateFormData(formData) {
-  if (!formData) {
+  if (
+    formData &&
+    formData.name &&
+    formData.city &&
+    !isNaN(parseInt(formData.zipCode)) &&
+    document.getElementById("status").checked === true
+  ) {
+    return true;
+  } else {
     return false;
   }
-  
-  if (!isNumber(formData.zipCode)) {
-    return false;
-  }
-  if (!checkboxIsChecked()) {
-    return false;
-  }
-  return true;
 }
+
+const myForm = document.getElementById("my-form")
+
+function submit() {
+  const formData = handleGetFormData();
+
+  if (!validateFormData(formData)) {
+    const warning = document.getElementById("warning");
+    warning.textContent = "Periksa form anda sekali lagi";
+  } else {
+    const warning = document.getElementById("warning");
+    warning.textContent = "";
+    // myForm.submit();
+  }
+}
+
+myForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+  submit();
+})
